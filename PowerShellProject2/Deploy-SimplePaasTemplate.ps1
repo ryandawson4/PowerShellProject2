48a071be028a4114fc601b9b524f5a163032693e
Ryan Dawson <info@provenitservices.com>
2018-05-24 15:16:03 +0100

    push

---
+++ PowerShellProject2/Deploy-SimplePaasTemplate.ps1
@@ -0,0 +1,26 @@
+﻿### Define Deployment Variables
+{
+$location = 'UK South'
+$resourceGroupName = 'PITS-DATA-RG'
+$resourceDeploymentName = 'contoso-paas-template-deployment'
+$templatePath = "C:\Users\ryand\Source\Repos\pluralsight\microsoft-azure-resource-manager-mastering\"
+$templateFile = 'simplePaas.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+#{
+#New-AzureRmResourceGroup `
+#    -Name $resourceGroupName `
+#    -Location $location `
+#    -Verbose -Force
+#}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
